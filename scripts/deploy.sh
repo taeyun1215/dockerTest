4739216d644d97d687b37ee870322651f71e3dd5
taeyun1215 <taeyun1215@naver.com>
2022-12-07 19:59:11 +0900

    jenkins test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,13 @@
 sudo docker ps -a -q --filter "name=mkc" | grep -q . && docker stop mkc && docker rm mkc | true
 
 # 기존 이미지 삭제
-sudo docker rmi taeyun1215/mkc
+sudo docker rmi taeyun1215/mkc:1.0
 
 # 도커허브 이미지 pull
-sudo docker pull taeyun1215/mkc
+sudo docker pull taeyun1215/mkc:1.0
 
 # 도커 run
-docker run -d -p 8085:8080 --name mkc taeyun1215/mkc
+docker run -d -p 8085:8080 --name mkc taeyun1215/mkc:1.0
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
