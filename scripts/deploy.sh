604b6f4880d4ea0073879dd4404a5bc1e2f79050
taeyun1215 <taeyun1215@naver.com>
2022-12-08 20:34:01 +0900

    docker test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,22 @@
 sudo systemctl start docker
+echo "11111"
 
 # 가동중인 mkc 도커 중단 및 삭제
 sudo docker ps -a -q --filter "name=mkc" | grep -q . && docker stop mkc && docker rm mkc | true
+echo "22222"
 
 # 기존 이미지 삭제
 sudo docker rmi taeyun1215/mkc
+echo "33333"
 
 # 도커허브 이미지 pull
 sudo docker pull taeyun1215/mkc
+echo "44444"
 
 # 도커 run
 sudo docker run -p 8080:8080 taeyun1215/mkc
+echo "55555"
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
+echo "66666"
