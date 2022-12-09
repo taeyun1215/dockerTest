be39eb5285339ab2bd94ca94cb09bc36caeb1d72
taeyun1215 <taeyun1215@naver.com>
2022-12-09 23:58:07 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,19 @@
 sudo systemctl start docker
 
 # 가동중인 mkc 도커 중단 및 삭제
-sudo docker ps -a -q --filter "name=mkc" | grep -q . && docker stop mkc && docker rm mkc | true
+docker ps -a -q --filter "name=mkc" | grep -q . && docker stop mkc && docker rm mkc | true
 
 # 기존 이미지 삭제
-sudo docker rmi taeyun1215/mkc
+docker rmi taeyun1215/mkc
 echo "11111"
 # sudo docker system prune -a --volumes
 
 # 도커허브 이미지 pull
-sudo docker pull taeyun1215/mkc
+docker pull taeyun1215/mkc
 echo "22222"
 
 # 도커 run
-sudo docker run -d -p 8080:8080 --name mkc taeyun1215/mkc
+docker run -d -p 8080:8080 --name mkc taeyun1215/mkc
 echo "33333"
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
